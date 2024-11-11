7edf805e2cce93114d5a5b95fcfe61c3808dcaa2
suvesh <suveshgurung2@gmail.com>
2024-11-11 23:10:03 +0545

    added while loop for listening anad sending multiple messagaes

--- client.c
+++ client.c
@@ -4,25 +4,33 @@ int main() {
 
   int clientSocketFD = initial_client_socket_operation();
 
-  message input = input_message();
-  if (input.buffer == NULL) {
-    perror("input_message");
-    return 7;
+  printf("Enter text to send to the server, type 'exit' to exit\n\n");
+  message input;
+  while (1) {
+    input = input_message();
+    if (input.buffer == NULL) {
+      perror("input_message");
+      return 7;
+    }
+
+    if (!strcmp(input.buffer, "exit\r\n")) {
+      break;
+    }
+
+    ssize_t bytesSent;
+    if ((bytesSent = send(clientSocketFD, input.buffer, input.len, 0)) == -1) {
+      perror("send");
+      return 5;
+    }
   }
 
-  ssize_t bytesSent;
-  if ((bytesSent = send(clientSocketFD, input.buffer, input.len, 0)) == -1) {
-    perror("send");
-    return 5;
-  }
-
-  char buf[1024];
-  if (recv(clientSocketFD, buf, 1024, 0) == -1) {
-    perror("recv");
-    return 6;
-  }
-
-  printf("%s\n", buf);
+  // char buf[1024];
+  // if (recv(clientSocketFD, buf, 1024, 0) == -1) {
+  //   perror("recv");
+  //   return 6;
+  // }
+  //
+  // printf("%s\n", buf);
 
   close(clientSocketFD);
   free(input.buffer);
--- server.c
+++ server.c
@@ -1,26 +1,43 @@
 #include "utilities.h"
+#include <signal.h>
+
+int serverSocketFD;
+int clientSocketFD;
+char *recvBuf;
+
+void signalHandler(int sig);
 
 int main() {
   
-  int serverSocketFD;
-  int clientSocketFD = initial_server_socket_operation(&serverSocketFD);
+  clientSocketFD = initial_server_socket_operation(&serverSocketFD);
 
-  char *recvBuf;
-  if ((recvBuf = recv_data(clientSocketFD)) == NULL) {
-    perror("recvBuffer");
-    return 5;
+  signal(SIGINT, signalHandler);
+
+  while (1) {
+    if ((recvBuf = recv_data(clientSocketFD)) == NULL) {
+      perror("recvBuffer");
+      return 5;
+    }
+    printf("%s", recvBuf);
+
+    // char *sendBuf = "Hello from the server!";
+    // if (send(clientSocketFD, sendBuf, strlen(sendBuf), 0) == -1) {
+    //   perror("send");
+    //   return 6;
+    // }
   }
-  printf("%s\n", recvBuf);
 
-  char *sendBuf = "Hello from the server!";
-  if (send(clientSocketFD, sendBuf, strlen(sendBuf), 0) == -1) {
-    perror("send");
-    return 6;
-  }
+  close(clientSocketFD);
+  close(serverSocketFD);
+  free(recvBuf);
 
+  return 0;
+}
+
+void signalHandler(int sig) {
   close(serverSocketFD);
   close(clientSocketFD);
   free(recvBuf);
 
-  return 0;
+  exit(0);
 }
