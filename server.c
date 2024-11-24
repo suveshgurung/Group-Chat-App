734db5ed8228d8777cea29f2f66122a9b5cf2307
suvesh <suveshgurung2@gmail.com>
2024-11-24 22:09:24 +0545

    server shuts down successfully when SIGINT is sent

--- server.c
+++ server.c
@@ -1,45 +1,47 @@
 #include "utilities.h"
 #include <signal.h>
 #include <pthread.h>
+#include <stdbool.h>
 
 #define NUM_OF_THREADS 100    // number of users that can connect to the server.
 
-volatile int shutdownRequested = 0;
+typedef struct {
+  struct sockaddr_in address;
+  int serverSocketFD;
+} server_details;
+
+volatile bool shutdownRequested = false;
+volatile bool newClientConnection = false;
+
+int threadIndex = 0;
+pthread_t threadId[NUM_OF_THREADS];
+long clientSocketFDArr[NUM_OF_THREADS];
 
 void signalHandler(int sig);
 void *handleClientMessages(void *clientSocketFD);
+void *run_accept_thread(void *arg);
 
 int main() {
 
-  int serverSocketFD;
-  struct sockaddr_in address = initial_server_socket_operation(&serverSocketFD);
+  server_details server;
+  server.address = initial_server_socket_operation(&server.serverSocketFD);
 
   signal(SIGINT, signalHandler);
 
-  struct sockaddr_in clientSocketAddr;
-  socklen_t clientAddrLen = sizeof(clientSocketAddr);
-
-  int threadIndex = 0;
-  pthread_t threadId[NUM_OF_THREADS];
-  long clientSocketFDArr[NUM_OF_THREADS];
+  pthread_t accpetRunThreadId;
+  pthread_create(&accpetRunThreadId, NULL, run_accept_thread, (void *)&server);
 
   while (!shutdownRequested) {
-    // TODO: SIGINT pathauda accept is blocking. need to see this.
-    int clientSocketFD = accept(serverSocketFD, (struct sockaddr *)&address, &clientAddrLen);
-    if (clientSocketFD == -1) {
-      perror("accept");
-      exit(4);
+    if (newClientConnection) {
+      // create a new thread to handle the new client.
+      if (pthread_create(&threadId[threadIndex], NULL, handleClientMessages, (void *)clientSocketFDArr[threadIndex])) {
+        fprintf(stderr, "error in pthread_create\n");
+        return 6;
+      }
+
+      threadIndex++;
+      newClientConnection = false;
     }
-
-    // create a new thread to handle the new client.
-    clientSocketFDArr[threadIndex] = clientSocketFD;
-
-    if (pthread_create(&threadId[threadIndex], NULL, handleClientMessages, (void *)clientSocketFDArr[threadIndex])) {
-      fprintf(stderr, "error in pthread_create\n");
-      return 6;
-    }
-
-    threadIndex++;
   }
 
   // wait for all threads to terminate.
@@ -47,13 +49,13 @@ int main() {
     pthread_join(threadId[i], NULL);
   }
 
-  shutdown(serverSocketFD, SHUT_RDWR);
+  shutdown(server.serverSocketFD, SHUT_RDWR);
 
   return 0;
 }
 
 void signalHandler(int sig) {
-  shutdownRequested = 1;
+  shutdownRequested = true;
 }
 
 void *handleClientMessages(void *clientSocketFD) {
@@ -75,4 +77,25 @@ void *handleClientMessages(void *clientSocketFD) {
   close(socketFD);
   free(recvBuf);
   pthread_exit(NULL);
+}
+
+void *run_accept_thread(void *arg) {
+  server_details *serverDetails = (server_details *)(arg);
+
+  struct sockaddr_in clientSocketAddr;
+  socklen_t clientAddrLen = sizeof(clientSocketAddr);
+
+  while (true) {
+    int clientSocketFD = accept(serverDetails->serverSocketFD, (struct sockaddr *)&serverDetails->address, &clientAddrLen);
+    if (clientSocketFD == -1) {
+      perror("accept");
+      exit(4);
+    }
+
+    newClientConnection = true;
+
+    clientSocketFDArr[threadIndex] = clientSocketFD;
+  }
+
+  return NULL;
 }
